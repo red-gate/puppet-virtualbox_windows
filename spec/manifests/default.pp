e5a25613cfabf47ea83cf865fb3cf264c6bdbc4f
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-06-08 13:30:16 +0100

    tweaked the acceptance tests

--- spec/manifests/default.pp
+++ spec/manifests/default.pp
@@ -1,1 +1,22 @@
-include ::virtualbox_windows
+$virtualbox_vm_folder_path = 'D:/VirtualBox VMs'
+$dhcp_networkaddress = '172.55.127'
+
+include virtualbox_windows
+
+file { $virtualbox_vm_folder_path:
+  ensure  => directory,
+  require => Class['virtualbox_windows'],
+}
+->
+virtualbox_windows::property { 'hwvirtexclusive':
+  value => 'on',
+}
+->
+virtualbox_windows::property { 'machinefolder':
+  value   => $virtualbox_vm_folder_path,
+}
+
+class { '::virtualbox_windows::hostonly_network_adapter':
+  dhcp_networkaddress => $dhcp_networkaddress
+}
+Class['virtualbox_windows'] -> Class['virtualbox_windows::hostonly_network_adapter']
