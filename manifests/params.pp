74ad0b2a3a5cbb2f80ba3bdd736ad98fe324f993
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-06-09 09:58:47 +0100

    removed unused params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,5 @@
 # PRIVATE CLASS: do not use directly
 class virtualbox_windows::params {
-  # The path where the VirtualBox Vms will be saved to
-  $virtualbox_vm_folder_path = 'D:/VirtualBox VMs'
   $virtualbox_version        = '5.0.14.105127'
-  $vboxmanage                = 'C:/Program Files/Oracle/VirtualBox/VBoxManage.exe'
-  $hwvirtexclusive           = 'on'
   $vboxmanage_search_paths   = ['C:/Program Files/Oracle/VirtualBox']
 }
