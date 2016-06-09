e647a6afefb4a3675f454791de1818d13a628bf7
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-06-09 09:58:53 +0100

    whitespace change

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # Install virtualbox on windows.
 class virtualbox_windows(
-  $virtualbox_version        = $virtualbox_windows::params::virtualbox_version
+  $virtualbox_version = $virtualbox_windows::params::virtualbox_version
   ) inherits virtualbox_windows::params {
 
     package { 'virtualbox':
