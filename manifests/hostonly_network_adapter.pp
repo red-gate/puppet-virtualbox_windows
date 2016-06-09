d97302d9db4477e70277384aaa46a8f150ea5efc
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-06-09 09:16:17 +0100

    logoutput by default
    set the default paths for exec to 'C:/Program Files/Oracle/VirtualBox'
    simplify calls to vboxmanage.

--- manifests/hostonly_network_adapter.pp
+++ manifests/hostonly_network_adapter.pp
@@ -10,30 +10,34 @@ class virtualbox_windows::hostonly_network_adapter(
   $dhcp_upper_ip_range = "${dhcp_networkaddress}.254"
   $dhcp_netname = "HostInterfaceNetworking-${adapter_name}"
 
-  Exec { provider => 'powershell' }
+  Exec {
+    provider  => 'powershell',
+    logoutput => true,
+    path      => 'C:/Program Files/Oracle/VirtualBox',
+  }
 
   exec { 'Create VirtualBox host only adapter':
-    command => "& \"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe\" hostonlyif create",
+    command => 'VBoxManage hostonlyif create',
     onlyif  => template('virtualbox_windows/should_create_adapter.ps1.erb'),
   }
   ->
   exec { 'Virtualbox: vboxmanage dhcpserver add' :
-    command => "& \"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe\" dhcpserver add --netname \"${dhcp_netname}\" --ip ${dhcp_ipaddress} --netmask 255.255.255.0 --lowerip ${dhcp_lower_ip_range} --upperip ${dhcp_upper_ip_range} --enable",
+    command => "VBoxManage dhcpserver add --netname \"${dhcp_netname}\" --ip ${dhcp_ipaddress} --netmask 255.255.255.0 --lowerip ${dhcp_lower_ip_range} --upperip ${dhcp_upper_ip_range} --enable",
     onlyif  => template('virtualbox_windows/should_create_dhcp_server.ps1.erb'),
   }
   ->
   exec { 'Virtualbox: vboxmanage dhcpserver modify' :
-    command => "& \"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe\" dhcpserver modify --netname \"${dhcp_netname}\" --ip ${dhcp_ipaddress} --netmask 255.255.255.0 --lowerip ${dhcp_lower_ip_range} --upperip ${dhcp_upper_ip_range} --enable",
+    command => "VBoxManage dhcpserver modify --netname \"${dhcp_netname}\" --ip ${dhcp_ipaddress} --netmask 255.255.255.0 --lowerip ${dhcp_lower_ip_range} --upperip ${dhcp_upper_ip_range} --enable",
     onlyif  => template('virtualbox_windows/should_set_dhcp_server_properties.ps1.erb'),
   }
   ->
   exec { "Set VirtualBox Host-Only Ethernet Adapter IP to ${adapter_idaddress}" :
-    command => "& \"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe\" hostonlyif ipconfig \"VirtualBox Host-Only Ethernet Adapter\" --ip ${adapter_idaddress}",
+    command => "VBoxManage hostonlyif ipconfig \"VirtualBox Host-Only Ethernet Adapter\" --ip ${adapter_idaddress}",
     onlyif  => template('virtualbox_windows/should_set_adapter_properties.ps1.erb'),
   }
   ->
   exec { 'Set VirtualBox Host-Only Ethernet Adapter DHCP' :
-    command     => "& \"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe\" hostonlyif ipconfig \"VirtualBox Host-Only Ethernet Adapter\" --dhcp",
+    command     => "VBoxManage hostonlyif ipconfig \"VirtualBox Host-Only Ethernet Adapter\" --dhcp",
     refreshonly => true,
     subscribe   => Exec["Set VirtualBox Host-Only Ethernet Adapter IP to ${adapter_idaddress}"]
   }
