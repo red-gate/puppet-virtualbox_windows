212900190403cb24525f5d43a37e5363d63b3b54
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-10-31 14:40:56 +0000

    Use winrm v2 api

--- spec/acceptance/spec_windowshelper.rb
+++ spec/acceptance/spec_windowshelper.rb
@@ -4,8 +4,9 @@ require 'winrm'
 set :backend, :winrm
 set :os, :family => 'windows'
 
-endpoint = "http://#{ENV['KITCHEN_HOSTNAME']}:#{ENV['KITCHEN_PORT']}/wsman"
-username = ENV['KITCHEN_USERNAME']
-password = 'vagrant' #sadly, no KITCHEN_PASSWORD variable at the time of writing.
-
-Specinfra.configuration.winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => username, :pass => password, :basic_auth_only => true).create_executor
+opts = {
+  endpoint: "http://#{ENV['KITCHEN_HOSTNAME']}:#{ENV['KITCHEN_PORT']}/wsman",
+  user: ENV['KITCHEN_USERNAME'],
+  password: 'vagrant'
+}
+Specinfra.configuration.winrm = WinRM::Connection.new(opts)
