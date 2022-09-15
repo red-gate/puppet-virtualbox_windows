71f782742fadc3b182571632398fb8f9f48ddd09
Steve Brown <steve.brown+github@brownserve.co.uk>
2022-09-15 12:02:06 +0100

    Handle case where service no longer exists

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,9 +12,26 @@ class virtualbox_windows(
       install_options => ['--allow-empty-checksums'],
     }
 
-    # Make sure the Virtualbox kernel driver is running
-    -> service { 'vboxdrv':
-      ensure => 'running',
-      enable => true,
+    case versioncmp($virtualbox_version, '6.1.30')
+    {
+      1:
+      {
+        # If the version of VB is greater than version 6.1.30 then 'vboxdrv' no longer exists on Windows.
+        $manage_vbox_drv = false
+      }
+      default:
+      {
+        # If the version of VB is less than or equal to 6.1.30 then 'vboxdrv' does exist and we should manage it.
+        $manage_vbox_drv = trues
+      }
+    }
+    if ($manage_vbox_drv == true)
+    {
+      # Make sure the Virtualbox kernel driver is running
+      service { 'vboxdrv':
+        ensure  => 'running',
+        enable  => true,
+        require => Package['virtualbox']
+      }
     }
 }
